5d8707bff75bdf1ef79ed53f398338ea90c7de3e
Aasthajain96 <101544021+Aasthajain96@users.noreply.github.com>
2022-04-20 16:22:50 +0530

    Update Streamlit_app.py
    21. assigning mutliselected fruit to new variable ie. fruits_selected
    24. picking the selected fruit from whole fruit list ie.  my_fruit_list to new variable ie. fruits_to_show

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -18,7 +18,10 @@ my_fruit_list  = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.c
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Lets put up a list here so that they can pick up the fruit they want to include
-streamlit.multiselect('pickup fruits for your smoothie :',list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected = streamlit.multiselect('pickup fruits for your smoothie :',list(my_fruit_list.index),['Avocado','Strawberries'])
+
+#picking the selected fruit from the whole fruit list ie. my_fruit_list
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 #displaying the content of our dataframe 'my_fruit_list'
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
