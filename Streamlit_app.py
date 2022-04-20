c7903d8eea3dca9489578abcd17c854fafc6718d
Aasthajain96 <101544021+Aasthajain96@users.noreply.github.com>
2022-04-20 15:46:40 +0530

    Update Streamlit_app.py
    13. using the Panda function 'read_csv' to read the csv file and store it to data frame 'my_fruit_list'
    14. displaying the content store in our dataframe 'my_fruit_list'

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-
+import panda
 streamlit.title("My Mom's New Healthy Diner")
 streamlit.header('Breakfast Favorites')
 streamlit.text('Omega 3 & Plain Sandwich')
@@ -8,3 +8,7 @@ streamlit.text('Paratha with Curd & Lassi')
 streamlit.text('Avocado Sandwich')
 
 streamlit.header('Build your own fruit smoothie')
+
+
+my_fruit_list  = panda.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
